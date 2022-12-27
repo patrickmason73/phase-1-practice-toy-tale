8f2bef4a6f63dcacb7df2a4bdaf566308e0e92aa
Patrick Mason <patrickmason73@gmail.com>
2022-12-27 15:12:35 -0600

    commit tree 5366422750242654d08bf7a4c1b9343566ebc5a7

--- src/index.js
+++ src/index.js
@@ -13,3 +13,63 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(data => appendData(data))
+  function appendData(data) {
+    console.log(data)
+    const toycards = document.getElementById('toy-collection')
+    data.forEach(element => {
+    const card = document.createElement('div')
+    card.innerHTML = 
+    `<div class="card">
+    <h2>${element.name}</h2>
+    <img src=${element.image} class="toy-avatar" />
+    <p>${element.likes}</p>
+    <button class="like-btn" id=${element.id}>Like ❤️</button>
+  </div>`,
+  toycards.append(card) })}
+  const form = document.querySelector('form')
+  let toyName = document.getElementById('toyName')
+  let toyimgURL = document.getElementById('toyimgurl')
+  
+      function appendNewData(data) {
+        const toycards = document.getElementById('toy-collection')
+    const card = document.createElement('div')
+    card.innerHTML = 
+    `<div class="card">
+    <h2>${data.name}</h2>
+    <img src=${data.image} class="toy-avatar" />
+    <p>${data.likes}</p>
+    <button class="like-btn">Like ❤️</button>
+  </div>`,
+  toycards.append(card) 
+      }
+  
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(toyName.value);
+  fetch('http://localhost:3000/toys', {
+    method: 'POST', 
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+    "name": `${toyName.value}`,
+    "image": `${toyimgURL.value}`,
+    "likes": 0})
+  })
+  .then(res => res.json())
+    .then((data) => {
+      appendNewData(data)
+    })})
+  
+}
+  )
+  
+
+
